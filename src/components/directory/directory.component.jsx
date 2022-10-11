843fdf60ecbda8157bd6022036d16792c91ec94d
ABDELILAHelkrymy <abdelilah.elkrymy@usmba.ac.ma>
2022-10-11 16:13:52 +0100

    create categories component

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './directory.style.scss'
+import CategoryItem from '../category-item/category-item.component'
+
+
+const Directory = ({categories}) => {
+  return (
+    <div className="categories-container">
+
+      {categories.map(({id, title, imageUrl})=>(
+        <CategoryItem key={id} title={title} imageUrl={imageUrl}/>
+      ))}
+      
+    </div>
+  )
+}
+
+export default Directory
